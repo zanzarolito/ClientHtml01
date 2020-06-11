bbc2a034ff339eade5dcaf2084bc0a66c3abeff4
zanzarolito <arabesandratana@gmail.com>
2020-06-11 08:35:06 -0700

    ajout du script test.js (active le bouton hello)

---
+++ page_web/test.js
@@ -0,0 +1,23 @@
+var button = document.getElementById("bouton_bjr");
+var button2 = document.getElementById("bouton2");
+
+var titre = document.getElementById("title");
+
+button.addEventListener("click",function(){
+    console.log("bonjour!")
+});
+
+
+/*button2.addEventListener("click",function(){
+    
+    const http = new XMLHttpRequest()
+
+    http.open("GET", "")
+    http.send()
+
+    http.onload = () => console.log(http.responseText)
+
+
+});*/
+
+//alert(titre.innerHTML);
